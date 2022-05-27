f08fbf40cceaf2ff253b88b55ac67c3d238b6bf4
Sathwik Reddy Yasa <yasasathwikreddy@gmail.com>
2022-05-27 13:20:05 +0530

    E-Commerce API

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const authRoute = require('./routes/auth')
 const productRoute = require('./routes/product')
 const cartRoute = require('./routes/cart')
 const orderRoute = require('./routes/order')
+const stripeRoute = require('./routes/stripe')
 
 const url = process.env.DATABASE_URL
 mongoose.connect(url)
@@ -15,10 +16,13 @@ mongoose.connect(url)
     .catch(err => console.log(err))
 
 app.use(express.json())
+
+//ROUTES
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
 app.use('/api/products', productRoute)
 app.use('/api/carts', cartRoute)
 app.use('/api/orders', orderRoute)
+app.use('/api/checkout', stripeRoute)
 
 app.listen(process.env.PORT || 3000, () => console.log('listening on PORT 3000'))
